c3057ff771a30e72b845b23b79bdc2d1094f56e1
vaynedu <1219345363@qq.com>
2026-09-01 22:13:47 +0800

    refactor(hresty): 移除 internal/logger 依赖,仅暴露结构化 trace
    - 删除 PrintTraceInfo/PrintStructuredTrace,日志改由调用方负责
    - GetTraceInfo 处理 RemoteAddr nil 边界
    - 测试改用 httptest + goconvey,去掉外网依赖

--- pkg/hresty/resty_test.go
+++ pkg/hresty/resty_test.go
@@ -1,88 +1,45 @@
 package hresty
 
 import (
-	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
-	"time"
 
-	"github.com/avast/retry-go/v4"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestGet(t *testing.T) {
-	err := retry.Do(
-		func() error {
+func TestNewRestyClient(t *testing.T) {
+	Convey("NewRestyClient", t, func() {
+		client := NewRestyClient()
+		So(client, ShouldNotBeNil)
+		So(client.GetClient().Transport, ShouldNotBeNil)
+	})
+}
+
+func TestGetTraceInfo(t *testing.T) {
+	Convey("GetTraceInfo", t, func() {
+		Convey("nil 响应返回 error", func() {
+			ti, err := GetTraceInfo(nil)
+			So(err, ShouldNotBeNil)
+			So(ti, ShouldBeNil)
+		})
+
+		Convey("正常请求返回 trace", func() {
+			srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+				_, _ = w.Write([]byte("hello"))
+			}))
+			defer srv.Close()
+
 			client := NewRestyClient()
-			resp, err := client.R().EnableTrace().
-				SetQueryParam("name", "value").
-				SetQueryParams(map[string]string{
-					"vayne": "du",
-				}).
-				SetHeader("Content-Type", "application/json").
-				//Get("https://httpbin.org/get")
-				Get("https://www.qq.com/")
-			if err != nil {
-				t.Fatal(err)
-			}
-			if resp == nil {
-				t.Fatal("resp is nil")
-			}
+			resp, err := client.R().EnableTrace().Get(srv.URL)
+			So(err, ShouldBeNil)
+			So(resp.StatusCode(), ShouldEqual, http.StatusOK)
 
-			// 打印请求的url
-			t.Log(resp.Request.URL)
-			// 打印请求方法
-			t.Log(resp.Request.Method)
-			// 打印请求头
-			t.Log(resp.Request.Header)
-			// 打印请求体
-			// t.Log(resp.Request.Body)
-			// 打印响应头
-			t.Log(resp.Header())
-			// 打印响应体
-			// t.Log(resp.String())
-			// 打印trace信息
-			// 比较好的一张图，表示时间的关系
-			// https://vearne.cc/archives/39953
-			PrintTraceInfo(resp)
-			traceInfo := resp.Request.TraceInfo()
-			t.Logf("Reqeust trace info")
-			t.Logf("DNSLookup: %v", traceInfo.DNSLookup)
-			t.Logf("ConnTime: %v", traceInfo.ConnTime)
-			t.Logf("TCPConnTime: %v", traceInfo.TCPConnTime)
-			t.Logf("TLSHandshake: %v", traceInfo.TLSHandshake)
-			t.Logf("ServerTime: %v", traceInfo.ServerTime)
-			t.Logf("Responsetime: %v", traceInfo.ResponseTime)
-			t.Logf("TotalTime: %v", traceInfo.TotalTime)
-			t.Logf("Response size: %v", len(resp.Body()))
-			t.Logf("IsConnReused: %v", traceInfo.IsConnReused)
-			t.Logf("IsConnWasIdle: %v", traceInfo.IsConnWasIdle)
-			t.Logf("ConnIdleTime: %v", traceInfo.ConnIdleTime)
-			t.Logf("RequestAttempt: %v", traceInfo.RequestAttempt)
-			t.Logf("RemoteAddr: %v", traceInfo.RemoteAddr.String())
-
-			ti := traceInfo
-			fmt.Println("Request Trace Info:")
-			fmt.Println("DNSLookup:", ti.DNSLookup)
-			fmt.Println("ConnTime:", ti.ConnTime)
-			fmt.Println("TCPConnTime:", ti.TCPConnTime)
-			fmt.Println("TLSHandshake:", ti.TLSHandshake)
-			fmt.Println("ServerTime:", ti.ServerTime)
-			fmt.Println("ResponseTime:", ti.ResponseTime)
-			fmt.Println("TotalTime:", ti.TotalTime)
-			fmt.Println("IsConnReused:", ti.IsConnReused)
-			fmt.Println("IsConnWasIdle:", ti.IsConnWasIdle)
-			fmt.Println("ConnIdleTime:", ti.ConnIdleTime)
-			fmt.Println("RequestAttempt:", ti.RequestAttempt)
-			fmt.Println("RemoteAddr:", ti.RemoteAddr.String())
-
-			return nil
-		},
-		retry.Attempts(2),                   // 重试2次 3-1
-		retry.Delay(1*time.Second),          // 初始延时 1s
-		retry.DelayType(retry.BackOffDelay), // 指数退避策略（0.5s -> 1s）
-		retry.LastErrorOnly(true),           // 只返回最后一次错误
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-
+			trace, err := GetTraceInfo(resp)
+			So(err, ShouldBeNil)
+			So(trace, ShouldNotBeNil)
+			So(trace.ResponseSize, ShouldEqual, len("hello"))
+			So(trace.RemoteAddr, ShouldNotBeEmpty)
+		})
+	})
 }
